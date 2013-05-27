18347760ceb84084c56c3856dd8ad163e9e2b1e8
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-05-27 18:21:56 +0900

    Enhanced to save lines data to *.json.

--- src/jp/gr/java_conf/neko_daisuki/photonote/EditActivity.java
+++ src/jp/gr/java_conf/neko_daisuki/photonote/EditActivity.java
@@ -1,6 +1,12 @@
 package jp.gr.java_conf.neko_daisuki.photonote;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,6 +15,7 @@ import android.content.Intent;
 import android.graphics.PointF;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.JsonWriter;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -30,6 +37,7 @@ public class EditActivity extends Activity {
     private class OkeyButtonOnClickListener implements View.OnClickListener {
 
         public void onClick(View view) {
+            saveLines();
             setResult(RESULT_OK, getIntent());
             finish();
         }
@@ -104,10 +112,17 @@ public class EditActivity extends Activity {
         public int getLineColor(int line) {
             return mLines.get(line).getColor();
         }
+
+        public float getLineWidth(int line) {
+            return mLines.get(line).getStrokeWidth();
+        }
     }
+
+    private static final String LOG_TAG = "photonote";
 
     private PaintView mPaintView;
     private Adapter mAdapter;
+    private String mAdditionalPath;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -138,11 +153,73 @@ public class EditActivity extends Activity {
 
         Button okeyButton = (Button)findViewById(R.id.okey_button);
         okeyButton.setOnClickListener(new OkeyButtonOnClickListener());
+
+        mAdditionalPath = i.getStringExtra(Extra.ADDITIONAL_PATH.name());
     }
 
     private void setImage(int view, Intent i, Extra key) {
         ImageView v = (ImageView)findViewById(view);
         v.setImageURI(Uri.fromFile(new File(i.getStringExtra(key.name()))));
+    }
+
+    private void saveLinesToJson(JsonWriter writer) throws IOException {
+        writer.beginArray();
+        int nLines = mAdapter.getLineCount();
+        for (int i = 0; i < nLines; i++) {
+            writer.beginObject();
+            writer.name("color").value(mAdapter.getLineColor(i));
+            writer.name("width").value(mAdapter.getLineWidth(i));
+            writer.name("points");
+            writer.beginArray();
+            int nPoints = mAdapter.getPointCount(i);
+            for (int j = 0; j < nPoints; j++) {
+                PointF point = mAdapter.getPoint(i, j);
+                writer.beginObject();
+                writer.name("x").value(point.x);
+                writer.name("y").value(point.y);
+                writer.endObject();
+            }
+            writer.endArray();
+            writer.endObject();
+        }
+        writer.endArray();
+    }
+
+    private void saveLines() {
+        OutputStream out;
+        try {
+            out = new FileOutputStream(mAdditionalPath);
+        }
+        catch (IOException e) {
+            String fmt = "failed to open %s: %s";
+            Log.e(LOG_TAG, String.format(fmt, mAdditionalPath, e.getMessage()));
+            return;
+        }
+        String encoding = "UTF-8";
+        Writer writer;
+        try {
+            writer = new OutputStreamWriter(out, encoding);
+        }
+        catch (UnsupportedEncodingException e) {
+            String fmt = "failed to write %s with encoding %s: %s";
+            String message = e.getMessage();
+            Log.e(LOG_TAG,
+                    String.format(fmt, mAdditionalPath, encoding, message));
+            return;
+        }
+        JsonWriter jsonWriter = new JsonWriter(writer);
+        try {
+            try {
+                saveLinesToJson(jsonWriter);
+            }
+            finally {
+                jsonWriter.close();
+            }
+        }
+        catch (IOException e) {
+            String fmt = "failed to write %s: %s";
+            Log.e(LOG_TAG, String.format(fmt, mAdditionalPath, e.getMessage()));
+        }
     }
 }
 
