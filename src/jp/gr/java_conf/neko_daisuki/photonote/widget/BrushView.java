783ab949705a838734b12f2f82bdaca970b98725
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-05-29 01:23:17 +0900

    Enhanced to draw brushes.

--- src/jp/gr/java_conf/neko_daisuki/photonote/widget/BrushView.java
+++ src/jp/gr/java_conf/neko_daisuki/photonote/widget/BrushView.java
@@ -2,17 +2,26 @@ package jp.gr.java_conf.neko_daisuki.photonote.widget;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint.Style;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Region.Op;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class BrushView extends View {
 
-    // documents
+    private int mSelected;
+
+    /**
+     * Array of stroke width. Width must be in this array in descendent order.
+     */
     private int[] mWidth;
 
     // drawing data
-    private int mSize = 48;
+    private int mBorderWidth = 8;
 
     public BrushView(Context context) {
         super(context);
@@ -35,18 +44,63 @@ public class BrushView extends View {
     }
 
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        setMeasuredDimension(mSize, mSize * mWidth.length);
+        int size = getCellSize();
+        setMeasuredDimension(size, size * mWidth.length);
     }
 
     protected void onDraw(Canvas canvas) {
-        // TODO
+        int width = getWidth();
+        Rect rect = new Rect(0, 0, width - 1, getHeight() - 1);
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Style.FILL);
+        canvas.drawRect(rect, paint);
+
+        paint.setColor(Color.BLACK);
+        int size = getCellSize();
+        float x = (float)width / 2;
+        for (int i = 0; i < mWidth.length; i++) {
+            float y = (float)(i * size + size / 2);
+            canvas.drawCircle(x, y, (float)(mWidth[i] / 2), paint);
+        }
+
+        paint.setStrokeWidth(1.0f);
+        paint.setStyle(Style.STROKE);
+        canvas.drawRect(rect, paint);
+        for (int i = 1; i < mWidth.length; i++) {
+            float y = (float)(i * size);
+            canvas.drawLine(0.0f, y, (float)width, y, paint);
+        }
+
+        Rect cellRect = new Rect();
+        cellRect.left = 0;
+        cellRect.top = mSelected * size;
+        cellRect.right = width - 1;
+        cellRect.bottom = rect.top + size - 1;
+        canvas.clipRect(cellRect, Op.REPLACE);
+        rect.left = cellRect.left + mBorderWidth;
+        rect.top = cellRect.top + mBorderWidth;
+        rect.right = cellRect.right - mBorderWidth;
+        rect.bottom = cellRect.bottom - mBorderWidth;
+        canvas.clipRect(rect, Op.DIFFERENCE);
+        paint.setStyle(Style.FILL);
+        canvas.drawRect(cellRect, paint);
+    }
+
+    private int getMaxWidth() {
+        return mWidth[0];
+    }
+
+    private int getCellSize() {
+        return 2 * mBorderWidth + getMaxWidth();
     }
 
     private void initialize() {
         setFocusable(true);
         setFocusableInTouchMode(true);
 
-        mWidth = new int[] { 48 };
+        mSelected = 0;
+        mWidth = new int[] { 96, 48, 32, 24, 16, 8 };
     }
 }
 
