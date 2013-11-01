05b12189536189f547a22d3461a94eeb3d72c4eb
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-11-01 18:37:48 +0900

    Fixed not to crash after deleting an entry.
    In the previous version, even when an entry is deleted, the application
    did not update the list in .photonote/groups/<group_name>. So if a user
    does an operation which reads data again from .photonote (these
    operations are deleting a group and adding a group) immediately after
    deleting an entry, the application crashed.
    
    This patch changes to write all groups after deleting an entry. This is
    not the best. The best is updating only the group of the deleted entry.

--- src/jp/gr/java_conf/neko_daisuki/photonote/MainActivity.java
+++ src/jp/gr/java_conf/neko_daisuki/photonote/MainActivity.java
@@ -133,6 +133,7 @@ public class MainActivity extends Activity {
 
         public void onClick(DialogInterface dialog, int which) {
             deleteEntry(mDeletingEntry);
+            saveGroups();
             updateList();
         }
     }
